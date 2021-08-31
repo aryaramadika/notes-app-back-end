fe1e02f9d6294211ced17b9b34c6c1174d3ab4df
Rama <aryaramadika78@gmail.com>
2021-09-01 01:33:46 +0800

    error code

---
+++ migrations/1630423493677_create-table-notes.js
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('notes',{
+        id: {
+            type:'VARCHAR(50)',
+            primaryKey: true,
+        },
+        body:{
+            type : 'TEXT',
+            notNull: true,
+        },
+        tags: {
+            type : 'Text[]',
+            notNull : true,
+        },
+        created_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        updated_at: {
+            type: 'TEXT',
+            notNull: true,
+          },
+
+    });
+};
+
+exports.down = pgm => {pgm.dropTable('notes');};
